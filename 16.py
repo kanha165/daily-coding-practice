c581bedbcfe70e1e6caa7c5053f60f9480261e20
kanha patidar <kanhapatidar7251@gmail.com>
2026-03-30 10:51:18 +0530

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,8 @@
+#minimun number in a list
+l=[12,34,56,6,897,64,34,24,42,45,21,3,87,43]
+minimun=l[0]
+for i in l:
+    if i<minimun:
+        minimun=i
+print("minimun number in a list is :",minimun)       
+        
