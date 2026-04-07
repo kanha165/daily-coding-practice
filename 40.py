6317ad1d6a253eb933a6dc5d9416ef9495cff87f
kanha <kanhapatidar7251@gmail.com>
2026-04-07 16:01:04 +0530

    update list

--- 40.py
+++ 40.py
@@ -1,5 +1,5 @@
-# Shift all 0 of the list to the end
-list=[1,2,0,3,4,0,4,0,4,5,0,0,9,0,9,0,7,6,5,4,9,0]
+# Shift all 0 of the list to the end 
+list=[1,2,0,3,4,0,4,0,4,5,0,0,9,0,9,0,7,6,5,4,5,2,9,0]
 new=[]
 for i in range(len(list)):
     if list[i]!=0:
