cde2a0213662ba8b99a544f64a32929463c69eaf
kanha patidar <kanhapatidar7251@gmail.com>
2026-04-10 15:54:10 +0530

    Delete 49.py

--- 49.py
+++
@@ -1,7 +0,0 @@
-# Find intersection of two arrays
-from array import array
-arr=array('i',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19])
-arr2=array('i',[1,4,5,6,7,8,9])
-for i in arr:
-    if i in arr2:
-        print("Common element",i)
